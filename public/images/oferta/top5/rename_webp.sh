d095a78b00efbbf5fb36b2193cee25f1e10bbe95
Aleksander Jakowczuk <aleksanderjakowczuk@62201afc-e667-4b23-8b45-52ddf7442ab7.fritz.box>
2025-05-28 20:38:54 +0200

    fix: add .webp images for oferta top5 section

--- public/images/oferta/top5/rename_webp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in *.webp; do
-  newname=$(echo "$file" | tr '[:upper:]' '[:lower:]' | sed -E 's/[()]+//g' | sed -E 's/[ ]+/-/g' | sed -E 's/--+/-/g')
-  mv "$file" "$newname"
-  echo "$file => $newname"
-done
-
